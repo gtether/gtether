ab0c8edff31679f7c691abbfd9b66b3fbdd491d4
James M De Broeck <debroejm@gmail.com>
2024-05-14 15:05:21 -0700

    examples: Add a 'windows' example
    Add an example that shows window creation and management, and does some
    simple rendering.
    
    Signed-off-by: James M De Broeck <debroejm@gmail.com>

---
+++ examples/windows/src/main.rs
@@ -0,0 +1,114 @@
+extern crate nalgebra_glm as glm;
+
+use std::{thread, time};
+
+use glm::identity;
+use vulkano::format::Format;
+use vulkano::image::SampleCount;
+use vulkano::render_pass::{AttachmentDescription, AttachmentLoadOp, AttachmentStoreOp};
+
+use gtether::{Engine, EngineMetadata};
+use gtether::render::render_pass::{EngineRenderPassBuilder};
+use gtether::render::window::CreateWindowInfo;
+
+use crate::render::{MN, VP};
+use crate::render::ambient::AmbientRenderer;
+use crate::render::cube::CubeRenderer;
+use crate::render::directional::{DirectionalRenderer, PointLight};
+
+mod render;
+
+fn main() {
+    let engine = Engine::new(
+        EngineMetadata {
+            application_name: Some(String::from("gTether Example - windows")),
+            ..Default::default()
+        }
+    );
+
+    println!("Engine started");
+
+    let mut model = identity();
+    model = glm::translate(&model, &glm::vec3(0.0, 0.0, -5.0));
+    model = glm::rotate_normalized_axis(&model, 1.0, &glm::vec3(0.0, 1.0, 0.0));
+    model = glm::rotate_normalized_axis(&model, 1.0, &glm::vec3(1.0, 0.0, 0.0));
+    model = glm::rotate_normalized_axis(&model, 1.0, &glm::vec3(0.0, 0.0, 1.0));
+
+    let window = engine.window_manager().create_window(CreateWindowInfo {
+        title: Some("Windowing Test".into()),
+        ..Default::default()
+    });
+    println!("Window created");
+
+    let cube_renderer = CubeRenderer::new(window.render_target());
+    let mn = cube_renderer.mn().clone();
+    mn.set(MN::new(model));
+    let vp = cube_renderer.vp().clone();
+    vp.set(VP::look_at(
+        &glm::vec3(0.0, 0.0, 0.1),
+        &glm::vec3(0.0, 0.0, 0.0),
+        &glm::vec3(0.0, 1.0, 0.0),
+    ));
+
+    let ambient_renderer = AmbientRenderer::new(window.render_target());
+    // let ambient_light = ambient_renderer.light().clone();
+
+    let directional_renderer = DirectionalRenderer::new(window.render_target());
+    let point_lights = directional_renderer.lights().clone();
+    point_lights.set(vec![
+        PointLight {
+            position: [-4.0, -4.0, 0.0, 1.0],
+            color: [1.0, 1.0, 1.0],
+        },
+    ]);
+
+    let render_pass = EngineRenderPassBuilder::new(window.render_target())
+        .attachment("color".into(), AttachmentDescription {
+            format: Format::A2B10G10R10_UNORM_PACK32,
+            samples: SampleCount::Sample1,
+            load_op: AttachmentLoadOp::Clear,
+            store_op: AttachmentStoreOp::DontCare,
+            ..Default::default()
+        }, Some([0.0, 0.0, 0.0, 1.0].into()))
+        .attachment("normals".into(), AttachmentDescription {
+            format: Format::R16G16B16A16_SFLOAT,
+            samples: SampleCount::Sample1,
+            load_op: AttachmentLoadOp::Clear,
+            store_op: AttachmentStoreOp::DontCare,
+            ..Default::default()
+        }, Some([0.0, 0.0, 0.0, 1.0].into()))
+        .attachment("depth".into(), AttachmentDescription {
+            format: Format::D16_UNORM,
+            samples: SampleCount::Sample1,
+            load_op: AttachmentLoadOp::Clear,
+            store_op: AttachmentStoreOp::DontCare,
+            ..Default::default()
+        }, Some(1.0.into()))
+        .final_color_attachment("final_color".into(), [0.0, 0.0, 0.0, 1.0].into())
+        .begin_subpass()
+        .color_attachment("color".into())
+        .color_attachment("normals".into())
+        .depth_stencil_attachment("depth".into())
+        .handler(Box::new(cube_renderer))
+        .end_subpass()
+        .begin_subpass()
+        .input_attachment("color".into())
+        .input_attachment("normals".into())
+        .color_attachment("final_color".into())
+        .handler(Box::new(ambient_renderer))
+        .handler(Box::new(directional_renderer))
+        .end_subpass()
+        .build();
+    window.set_render_pass(render_pass);
+    println!("RenderPass initialized");
+
+    while engine.window_manager().get_window_count() > 0 {
+        let model = mn.get();
+        mn.set(MN::new(glm::rotate_normalized_axis(&model.model, 0.01, &glm::vec3(0.0, 0.0, 1.0))));
+
+        thread::sleep(time::Duration::from_secs_f32(1.0 / 60.0));
+    }
+    println!("All windows closed, exiting");
+
+    engine.exit();
+}
---
+++ examples/windows/src/render/cube.rs
@@ -0,0 +1,226 @@
+use std::sync::Arc;
+use vulkano::buffer::{Buffer, BufferContents, BufferCreateInfo, BufferUsage, Subbuffer};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, PrimaryAutoCommandBuffer};
+use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter};
+use vulkano::pipeline::graphics::vertex_input::{Vertex, VertexDefinition};
+use vulkano::pipeline::{GraphicsPipeline, Pipeline, PipelineBindPoint, PipelineLayout, PipelineShaderStageCreateInfo};
+use vulkano::pipeline::graphics::color_blend::{ColorBlendAttachmentState, ColorBlendState};
+use vulkano::pipeline::graphics::depth_stencil::{DepthState, DepthStencilState};
+use vulkano::pipeline::graphics::GraphicsPipelineCreateInfo;
+use vulkano::pipeline::graphics::input_assembly::InputAssemblyState;
+use vulkano::pipeline::graphics::multisample::MultisampleState;
+use vulkano::pipeline::graphics::rasterization::{CullMode, RasterizationState};
+use vulkano::pipeline::graphics::viewport::{Viewport, ViewportState};
+use vulkano::pipeline::layout::PipelineDescriptorSetLayoutCreateInfo;
+use vulkano::render_pass::Subpass;
+use gtether::render::render_pass::{AttachmentMap, EngineRenderHandler};
+use gtether::render::RenderTarget;
+use gtether::render::swapchain::Framebuffer;
+use crate::render::{MN, Uniform, VP};
+
+mod deferred_vert {
+    vulkano_shaders::shader! {
+        ty:  "vertex",
+        path: "src/shaders/deferred.vert",
+    }
+}
+
+mod deferred_frag {
+    vulkano_shaders::shader! {
+        ty: "fragment",
+        path: "src/shaders/deferred.frag",
+    }
+}
+
+#[derive(BufferContents, Vertex)]
+#[repr(C)]
+struct CubeVertex {
+    #[format(R32G32B32_SFLOAT)]
+    position: [f32; 3],
+    #[format(R32G32B32_SFLOAT)]
+    normal: [f32; 3],
+    #[format(R32G32B32_SFLOAT)]
+    color: [f32; 3],
+}
+
+pub struct CubeRenderer {
+    target: Arc<dyn RenderTarget>,
+    graphics: Option<Arc<GraphicsPipeline>>,
+    vertex_buffer: Subbuffer<[CubeVertex]>,
+    mn: Arc<Uniform<MN>>,
+    vp: Arc<Uniform<VP>>,
+}
+
+impl CubeRenderer {
+    pub fn new(target: &Arc<dyn RenderTarget>) -> Self {
+        let color = [1.0, 0.35, 0.137];
+
+        let vertices = [
+            // Front
+            CubeVertex { position: [-1.0, -1.0,  1.0], normal: [0.0,  0.0,  1.0], color },
+            CubeVertex { position: [-1.0,  1.0,  1.0], normal: [0.0,  0.0,  1.0], color },
+            CubeVertex { position: [ 1.0,  1.0,  1.0], normal: [0.0,  0.0,  1.0], color },
+            CubeVertex { position: [-1.0, -1.0,  1.0], normal: [0.0,  0.0,  1.0], color },
+            CubeVertex { position: [ 1.0,  1.0,  1.0], normal: [0.0,  0.0,  1.0], color },
+            CubeVertex { position: [ 1.0, -1.0,  1.0], normal: [0.0,  0.0,  1.0], color },
+            // Back
+            CubeVertex { position: [ 1.0, -1.0, -1.0], normal: [ 0.0,  0.0, -1.0], color },
+            CubeVertex { position: [ 1.0,  1.0, -1.0], normal: [ 0.0,  0.0, -1.0], color },
+            CubeVertex { position: [-1.0,  1.0, -1.0], normal: [ 0.0,  0.0, -1.0], color },
+            CubeVertex { position: [ 1.0, -1.0, -1.0], normal: [ 0.0,  0.0, -1.0], color },
+            CubeVertex { position: [-1.0,  1.0, -1.0], normal: [ 0.0,  0.0, -1.0], color },
+            CubeVertex { position: [-1.0, -1.0, -1.0], normal: [ 0.0,  0.0, -1.0], color },
+            // Top
+            CubeVertex { position: [-1.0, -1.0,  1.0], normal: [ 0.0, -1.0,  0.0], color },
+            CubeVertex { position: [ 1.0, -1.0,  1.0], normal: [ 0.0, -1.0,  0.0], color },
+            CubeVertex { position: [ 1.0, -1.0, -1.0], normal: [ 0.0, -1.0,  0.0], color },
+            CubeVertex { position: [-1.0, -1.0,  1.0], normal: [ 0.0, -1.0,  0.0], color },
+            CubeVertex { position: [ 1.0, -1.0, -1.0], normal: [ 0.0, -1.0,  0.0], color },
+            CubeVertex { position: [-1.0, -1.0, -1.0], normal: [ 0.0, -1.0,  0.0], color },
+            // Bottom
+            CubeVertex { position: [ 1.0,  1.0,  1.0], normal: [ 0.0,  1.0,  0.0], color },
+            CubeVertex { position: [-1.0,  1.0,  1.0], normal: [ 0.0,  1.0,  0.0], color },
+            CubeVertex { position: [-1.0,  1.0, -1.0], normal: [ 0.0,  1.0,  0.0], color },
+            CubeVertex { position: [ 1.0,  1.0,  1.0], normal: [ 0.0,  1.0,  0.0], color },
+            CubeVertex { position: [-1.0,  1.0, -1.0], normal: [ 0.0,  1.0,  0.0], color },
+            CubeVertex { position: [ 1.0,  1.0, -1.0], normal: [ 0.0,  1.0,  0.0], color },
+            // Left
+            CubeVertex { position: [-1.0, -1.0, -1.0], normal: [-1.0,  0.0,  0.0], color },
+            CubeVertex { position: [-1.0,  1.0, -1.0], normal: [-1.0,  0.0,  0.0], color },
+            CubeVertex { position: [-1.0,  1.0,  1.0], normal: [-1.0,  0.0,  0.0], color },
+            CubeVertex { position: [-1.0, -1.0, -1.0], normal: [-1.0,  0.0,  0.0], color },
+            CubeVertex { position: [-1.0,  1.0,  1.0], normal: [-1.0,  0.0,  0.0], color },
+            CubeVertex { position: [-1.0, -1.0,  1.0], normal: [-1.0,  0.0,  0.0], color },
+            // Right
+            CubeVertex { position: [ 1.0, -1.0,  1.0], normal: [ 1.0,  0.0,  0.0], color },
+            CubeVertex { position: [ 1.0,  1.0,  1.0], normal: [ 1.0,  0.0,  0.0], color },
+            CubeVertex { position: [ 1.0,  1.0, -1.0], normal: [ 1.0,  0.0,  0.0], color },
+            CubeVertex { position: [ 1.0, -1.0,  1.0], normal: [ 1.0,  0.0,  0.0], color },
+            CubeVertex { position: [ 1.0,  1.0, -1.0], normal: [ 1.0,  0.0,  0.0], color },
+            CubeVertex { position: [ 1.0, -1.0, -1.0], normal: [ 1.0,  0.0,  0.0], color },
+        ];
+
+        let vertex_buffer = Buffer::from_iter(
+            target.device().memory_allocator().clone(),
+            BufferCreateInfo {
+                usage: BufferUsage::VERTEX_BUFFER,
+                ..Default::default()
+            },
+            AllocationCreateInfo {
+                memory_type_filter: MemoryTypeFilter::PREFER_DEVICE | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+                ..Default::default()
+            },
+            vertices,
+        ).unwrap();
+
+        CubeRenderer {
+            target: target.clone(),
+            graphics: None,
+            vertex_buffer,
+            mn: Arc::new(Uniform::new(MN::default(), target, 1)),
+            vp: Arc::new(Uniform::new(VP::default(), target, 0)),
+        }
+    }
+
+    #[inline]
+    pub fn mn(&self) -> &Arc<Uniform<MN>> { &self.mn }
+
+    #[inline]
+    pub fn vp(&self) -> &Arc<Uniform<VP>> { &self.vp }
+}
+
+impl EngineRenderHandler for CubeRenderer {
+    fn build_commands(&self, builder: &mut AutoCommandBufferBuilder<PrimaryAutoCommandBuffer>, _frame: &Framebuffer) {
+        let graphics = self.graphics.as_ref()
+            .expect(".recreate() not called yet");
+
+        builder
+            .bind_pipeline_graphics(graphics.clone()).unwrap()
+            .bind_descriptor_sets(
+                PipelineBindPoint::Graphics,
+                graphics.layout().clone(),
+                0,
+                (
+                    self.vp.descriptor_set().clone(),
+                    self.mn.descriptor_set().clone(),
+                ),
+            ).unwrap()
+            .bind_vertex_buffers(0, self.vertex_buffer.clone()).unwrap()
+            .draw(self.vertex_buffer.len() as u32, 1, 0, 0).unwrap();
+    }
+
+    fn recreate(&mut self, target: &Arc<dyn RenderTarget>, subpass: &Subpass, _attachments: &Arc<dyn AttachmentMap>) {
+        self.target = target.clone();
+
+        let viewport = Viewport {
+            offset: [0.0, 0.0],
+            extent: target.dimensions().into(),
+            depth_range: 0.0..=1.0,
+        };
+
+        let deferred_vert = deferred_vert::load(target.device().vk_device().clone())
+            .expect("Failed to create vertex shader module")
+            .entry_point("main").unwrap();
+
+        let deferred_frag = deferred_frag::load(target.device().vk_device().clone())
+            .expect("Failed to create fragment shader module")
+            .entry_point("main").unwrap();
+
+        let vertex_input_state = Some(CubeVertex::per_vertex()
+            .definition(&deferred_vert.info().input_interface)
+            .unwrap());
+
+        let stages = [
+            PipelineShaderStageCreateInfo::new(deferred_vert),
+            PipelineShaderStageCreateInfo::new(deferred_frag),
+        ];
+
+        let layout = PipelineLayout::new(
+            target.device().vk_device().clone(),
+            PipelineDescriptorSetLayoutCreateInfo::from_stages(&stages)
+                .into_pipeline_layout_create_info(target.device().vk_device().clone())
+                .unwrap(),
+        ).unwrap();
+
+        let graphics = GraphicsPipeline::new(
+            target.device().vk_device().clone(),
+            None,
+            GraphicsPipelineCreateInfo {
+                viewport_state: Some(ViewportState {
+                    viewports: [viewport].into_iter().collect(),
+                    ..Default::default()
+                }),
+                color_blend_state: Some(ColorBlendState::with_attachment_states(
+                    subpass.num_color_attachments(),
+                    ColorBlendAttachmentState::default(),
+                )),
+                depth_stencil_state: Some(DepthStencilState {
+                    depth: Some(DepthState::simple()),
+                    ..Default::default()
+                }),
+                subpass: Some(subpass.clone().into()),
+                stages: stages.into_iter().collect(),
+                vertex_input_state,
+                input_assembly_state: Some(InputAssemblyState::default()),
+                rasterization_state: Some(RasterizationState {
+                    cull_mode: CullMode::Back,
+                    ..Default::default()
+                }),
+                multisample_state: Some(MultisampleState::default()),
+                ..GraphicsPipelineCreateInfo::layout(layout)
+            }
+        ).unwrap();
+
+        let mut vp = self.vp.get();
+        vp.projection = glm::perspective(
+            target.dimensions().aspect_ratio(),
+            glm::half_pi(),
+            0.01, 100.0,
+        );
+        self.vp.set(vp);
+
+        self.mn.recreate(target, &graphics);
+        self.vp.recreate(target, &graphics);
+        self.graphics = Some(graphics);
+    }
+}
