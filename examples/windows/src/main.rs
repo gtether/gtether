4c780a487e16528c24023e5bec1523e292774c56
James M De Broeck <debroejm@gmail.com>
2024-06-12 18:29:01 -0700

    examples: Add console to windows example
    Add a Console to the windows example, and add some basic commands.
    
    Signed-off-by: James M De Broeck <debroejm@gmail.com>

--- examples/windows/src/main.rs
+++ examples/windows/src/main.rs
@@ -5,20 +5,129 @@ use std::sync::Arc;
 use std::time::Duration;
 
 use glm::identity;
+use tracing::{event, Level};
 use vulkano::format::Format;
 use vulkano::image::SampleCount;
 use vulkano::render_pass::{AttachmentDescription, AttachmentLoadOp, AttachmentStoreOp};
 
-use gtether::{Engine, EngineBuilder, EngineMetadata, Application, Registry};
-use gtether::render::render_pass::EngineRenderPassBuilder;
+use gtether::{Application, Engine, EngineBuilder, EngineMetadata, Registry};
+use gtether::console::{Console, ConsoleStdinReader};
+use gtether::console::command::{Command, CommandError, CommandRegistry, CommandTree, ParamCountCheck};
 use gtether::gui::window::{CreateWindowInfo, WindowAttributes};
+use gtether::render::render_pass::EngineRenderPassBuilder;
 
-use crate::render::{MN, Uniform, VP};
-use crate::render::ambient::AmbientRenderer;
+use crate::render::{MN, Uniform, UniformSet, VP};
+use crate::render::ambient::{AmbientLight, AmbientRenderer};
 use crate::render::cube::CubeRenderer;
 use crate::render::directional::{DirectionalRenderer, PointLight};
 
 mod render;
+
+#[derive(Debug)]
+struct AmbientLightCommand {
+    ambient_light: Arc<Uniform<AmbientLight>>,
+}
+
+impl Command for AmbientLightCommand {
+    fn handle(&self, parameters: &[String]) -> Result<(), CommandError> {
+        ParamCountCheck::Equal(3).check(parameters.len() as u32)?;
+
+        let r: f32 = parameters[0].parse()
+            .map_err(|e| CommandError::CommandFailure(Box::new(e)))?;
+        let g: f32 = parameters[1].parse()
+            .map_err(|e| CommandError::CommandFailure(Box::new(e)))?;
+        let b: f32 = parameters[2].parse()
+            .map_err(|e| CommandError::CommandFailure(Box::new(e)))?;
+
+        self.ambient_light.set(AmbientLight::new(r, g, b, 1.0));
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+struct PointLightListCommand {
+    point_lights: Arc<UniformSet<PointLight>>,
+}
+
+impl Command for PointLightListCommand {
+    fn handle(&self, parameters: &[String]) -> Result<(), CommandError> {
+        ParamCountCheck::Equal(0).check(parameters.len() as u32)?;
+
+        for (idx, point_light) in self.point_lights.get().into_iter().enumerate() {
+            event!(Level::INFO, "{idx}: {point_light:?}");
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+struct PointLightDeleteCommand {
+    point_lights: Arc<UniformSet<PointLight>>,
+}
+
+impl Command for PointLightDeleteCommand {
+    fn handle(&self, parameters: &[String]) -> Result<(), CommandError> {
+        ParamCountCheck::Equal(1).check(parameters.len() as u32)?;
+
+        let mut point_lights = self.point_lights.get();
+
+        let idx: usize = parameters[0].parse()
+            .map_err(|e| CommandError::CommandFailure(Box::new(e)))?;
+
+        if idx >= point_lights.len() {
+            return Err(CommandError::CommandFailure(format!("Index out of bounds: {idx}").as_str().into()))
+        }
+
+        point_lights.remove(idx);
+        self.point_lights.set(point_lights);
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+struct PointLightAddCommand {
+    point_lights: Arc<UniformSet<PointLight>>,
+}
+
+impl Command for PointLightAddCommand {
+    fn handle(&self, parameters: &[String]) -> Result<(), CommandError> {
+        ParamCountCheck::OneOf(vec![
+            ParamCountCheck::Equal(3),
+            ParamCountCheck::Equal(6),
+        ]).check(parameters.len() as u32)?;
+
+        let x: f32 = parameters[0].parse()
+            .map_err(|e| CommandError::CommandFailure(Box::new(e)))?;
+        let y: f32 = parameters[1].parse()
+            .map_err(|e| CommandError::CommandFailure(Box::new(e)))?;
+        let z: f32 = parameters[2].parse()
+            .map_err(|e| CommandError::CommandFailure(Box::new(e)))?;
+
+        let mut r: f32 = 1.0;
+        let mut g: f32 = 1.0;
+        let mut b: f32 = 1.0;
+        if parameters.len() == 6 {
+            r = parameters[3].parse()
+                .map_err(|e| CommandError::CommandFailure(Box::new(e)))?;
+            g = parameters[4].parse()
+                .map_err(|e| CommandError::CommandFailure(Box::new(e)))?;
+            b = parameters[5].parse()
+                .map_err(|e| CommandError::CommandFailure(Box::new(e)))?;
+        }
+
+        let mut point_lights = self.point_lights.get();
+        point_lights.push(PointLight {
+            position: [x, y, z, 1.0],
+            color: [r, g, b],
+        });
+        self.point_lights.set(point_lights);
+
+        Ok(())
+    }
+}
 
 struct AppCore {
     mn: OnceCell<Arc<Uniform<MN>>>,
@@ -34,6 +143,8 @@ impl AppCore {
 
 impl Application for AppCore {
     fn init(&self, _engine: &Engine<Self>, registry: &mut Registry) {
+        let mut console = Console::default();
+
         let mut model = identity();
         model = glm::translate(&model, &glm::vec3(0.0, 0.0, -5.0));
         model = glm::rotate_normalized_axis(&model, 1.0, &glm::vec3(0.0, 1.0, 0.0));
@@ -57,16 +168,37 @@ impl Application for AppCore {
         ));
 
         let ambient_renderer = AmbientRenderer::new(window.render_target());
-        // let ambient_light = ambient_renderer.light().clone();
+        let ambient_light = ambient_renderer.light().clone();
+        console.register_command("ambient", Box::new(AmbientLightCommand {
+            ambient_light,
+        })).unwrap();
 
         let directional_renderer = DirectionalRenderer::new(window.render_target());
         let point_lights = directional_renderer.lights().clone();
         point_lights.set(vec![
             PointLight {
                 position: [-4.0, -4.0, 0.0, 1.0],
-                color: [1.0, 1.0, 1.0],
+                color: [0.8, 0.8, 0.8],
             },
         ]);
+
+        let mut point_light_subcommands = CommandTree::default();
+        point_light_subcommands.register_command("list", Box::new(PointLightListCommand {
+            point_lights: point_lights.clone()
+        })).unwrap();
+        point_light_subcommands.register_alias("l", "list").unwrap();
+        point_light_subcommands.register_command("delete", Box::new(PointLightDeleteCommand {
+            point_lights: point_lights.clone()
+        })).unwrap();
+        point_light_subcommands.register_alias("del", "delete").unwrap();
+        point_light_subcommands.register_alias("d", "delete").unwrap();
+        point_light_subcommands.register_command("add", Box::new(PointLightAddCommand {
+            point_lights: point_lights.clone()
+        })).unwrap();
+        point_light_subcommands.register_alias("a", "add").unwrap();
+        console
+            .register_command("point", Box::new(point_light_subcommands)).unwrap()
+            .register_alias("points", "point").unwrap();
 
         let render_pass = EngineRenderPassBuilder::new(window.render_target())
             .attachment("color".into(), AttachmentDescription {
@@ -108,6 +240,9 @@ impl Application for AppCore {
         window.set_render_pass(render_pass);
 
         self.mn.set(mn).unwrap();
+
+        let console = Arc::new(console);
+        ConsoleStdinReader::start(&console);
     }
 
     fn tick(&self, _engine: &Engine<Self>, delta: Duration) {
